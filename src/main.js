4d37f1d7641026dadd3d1d15f267ae774d60fc41
Yao Zongyou <yaozongyou@vip.qq.com>
2024-11-09 15:44:09 +0800

    调通播放视频相关逻辑

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-const { invoke } = window.__TAURI__.core;
+const { invoke, convertFileSrc } = window.__TAURI__.core
 
 let greetInputEl;
 let greetMsgEl;
@@ -9,10 +9,29 @@ async function greet() {
 }
 
 window.addEventListener("DOMContentLoaded", () => {
+  /*
   greetInputEl = document.querySelector("#greet-input");
   greetMsgEl = document.querySelector("#greet-msg");
   document.querySelector("#greet-form").addEventListener("submit", (e) => {
     e.preventDefault();
     greet();
+  });
+  */
+
+  const video = document.getElementById('video_source');
+  const source = document.createElement('source');
+  source.type = 'video/mp4';
+  source.src = convertFileSrc('streaming_example_test_video.mp4', 'stream');
+  video.appendChild(source);
+  video.load();
+
+  const repeatReading = document.getElementById('repeat-reading');
+  repeatReading.addEventListener('click', (e) => {
+    e.preventDefault();
+    if (video.paused || video.ended) {
+      video.play();
+    } else {
+      video.pause();
+    }
   });
 });
